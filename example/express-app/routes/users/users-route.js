d1aa89dbc7c854561d5033ec1e91b49674da0d3b
Ryan Graham <r.m.graham@gmail.com>
2016-05-03 15:46:01 -0700

    update copyright and license

--- example/express-app/routes/users/users-route.js
+++ example/express-app/routes/users/users-route.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2013. All Rights Reserved.
+// Node module: strong-module-loader
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
 var MODULE_LOADER = '../../../../';
 var Module = require(MODULE_LOADER).Module;
 var inherits = require('util').inherits;
